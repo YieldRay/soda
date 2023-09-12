e40a0222359f8039912e988cd8c599e73a29842c
YieldRay <yieldray@outlook.com>
2023-09-12 17:48:39 +0800

    add switch

---
+++ src/components/Switch.tsx
@@ -0,0 +1,19 @@
+export function Switch(props: {
+    checked?: boolean
+    onChange?: (checked: boolean) => void
+    children?: React.ReactNode
+    disabled?: boolean
+}) {
+    return (
+        <div
+            className="sd-switch"
+            data-sd={props.disabled ? 'disabled' : 'enabled'}
+            data-sd-checked={props.checked ? 'true' : 'false'}
+            onClick={() => props.onChange?.(!props.checked)}
+        >
+            <div className="sd-switch-thumb">
+                <div className="sd-switch-icon">{props.children}</div>
+            </div>
+        </div>
+    )
+}
--- src/demo.tsx
+++ src/demo.tsx
@@ -9,6 +9,7 @@ function Demo() {
     const [value, setValue] = React.useState<string | number>('Edit me!')
     const [chip, setChip] = React.useState(false)
     const [radio, setRadio] = React.useState(false)
+    const [switcher, setSwitcher] = React.useState(false)
     return (
         <main
             style={{
@@ -75,7 +76,7 @@ function Demo() {
             <Soda.Chip
                 sd="tonal"
                 leadingIcon={'✨'}
-                trailingIcon={'╳'}
+                trailingIcon={'×'}
                 enabled={chip}
                 onClick={() => setChip(!chip)}
             >
@@ -98,6 +99,27 @@ function Demo() {
                 placeholder="placeholder"
                 sd="view"
             ></Soda.Search>
+
+            <Soda.Switch checked={switcher} onChange={setSwitcher}>
+                {switcher ? (
+                    <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+                            fill="currentColor"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                        ></path>
+                    </svg>
+                ) : (
+                    '×'
+                )}
+            </Soda.Switch>
         </main>
     )
 }
