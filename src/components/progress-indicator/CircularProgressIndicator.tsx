c31a5b15129c95ff9b357d9259ead6d6b4ad95dd
YieldRay <yieldray@outlook.com>
2023-12-20 19:26:11 +0800

    fix CircularProgressIndicator

--- src/components/progress-indicator/CircularProgressIndicator.tsx
+++ src/components/progress-indicator/CircularProgressIndicator.tsx
@@ -1,12 +1,14 @@
+import { ExtendProps } from '@/utils/type'
 import './progress-indicator.scss'
 import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react'
+import clsx from 'clsx'
 
 /**
  * @specs https://m3.material.io/components/progress-indicators/specs#c1e48032-c857-45d6-af1d-c5b5b0022cf4
  */
 export const CircularProgressIndicator = forwardRef<
     HTMLDivElement,
-    {
+    ExtendProps<{
         /**
          * Between 0 and 1, if is unset, act as undeterminate
          */
@@ -15,8 +17,11 @@ export const CircularProgressIndicator = forwardRef<
          * @default 36px
          */
         size?: string
-    }
->(function CircularProgressIndicator({ value, size }, ref) {
+    }>
+>(function CircularProgressIndicator(
+    { value, size, className, ...props },
+    ref
+) {
     const eRef = useRef<HTMLDivElement>(null)
     useImperativeHandle(ref, () => eRef.current!)
     useEffect(() => {
@@ -30,7 +35,8 @@ export const CircularProgressIndicator = forwardRef<
 
     return (
         <div
-            className="sd-circular_progress_indicator"
+            {...props}
+            className={clsx('sd-circular_progress_indicator', className)}
             ref={eRef}
             data-sd={
                 typeof value !== 'undefined' ? 'determinate' : 'undeterminate'
