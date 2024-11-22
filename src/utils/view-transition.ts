3540cbe648461b585c1cd51dbace6d09df46e871
YieldRay <yieldray@outlook.com>
2024-11-22 19:32:09 +0800

    fix view-transition types

--- src/utils/view-transition.ts
+++ src/utils/view-transition.ts
@@ -1,10 +1,6 @@
 import { flushSync } from 'react-dom'
 
-export const isViewTransitionSupported = Reflect.has(
-    document,
-    'startViewTransition',
-)
-
+export const isViewTransitionSupported = 'startViewTransition' in document
 /**
  * Not a `document.startViewTransition()` polyfill,
  * just do nothing in the browser that does not support this API,
@@ -25,7 +21,9 @@ export function startViewTransition(
     if (isViewTransitionSupported) {
         return document.startViewTransition(updateDOM)
     } else {
-        const updateCallbackDone = Promise.resolve(updateDOM()).then(() => {})
+        const updateCallbackDone = Promise.resolve(updateDOM()).then(
+            () => undefined,
+        )
         return {
             ready: Promise.reject(Error('View transitions unsupported')),
             updateCallbackDone,
@@ -51,37 +49,5 @@ export function startViewTransitionFlushSync(
         }).finished.finally(cleanup)
     } else {
         return callback()
-    }
-}
-
-declare global {
-    interface Document {
-        /**
-         * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Document/startViewTransition)
-         * @param callback A callback function typically invoked to update the DOM during the view transition process, which returns a Promise. The callback is invoked once the API has taken a screenshot of the current page. When the promise returned by the callback fulfills, the view transition begins in the next frame. If the promise returned by the callback rejects, the transition is abandoned.
-         * @returns A ViewTransition object instance.
-         */
-        startViewTransition(
-            callback: () => Promise<void> | void,
-        ): ViewTransition
-    }
-
-    interface ViewTransition {
-        /**
-         * A Promise that fulfills once the transition animation is finished, and the new page view is visible and interactive to the user.
-         */
-        finished: Promise<void>
-        /**
-         * A Promise that fulfills once the pseudo-element tree is created and the transition animation is about to start.
-         */
-        ready: Promise<void>
-        /**
-         * A Promise that fulfills when the promise returned by the document.startViewTransition()'s callback fulfills.
-         */
-        updateCallbackDone: Promise<void>
-        /**
-         * Skips the animation part of the view transition, but doesn't skip running the document.startViewTransition() callback that updates the DOM.
-         */
-        skipTransition(): void
     }
 }
