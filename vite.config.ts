53702b10d2f1336ced7540cffbf3980b5f062bf3
YieldRay <yieldray@outlook.com>
2025-03-26 17:07:50 +0800

    first implement of vite tsconfigAliasPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,54 @@
 import fs from 'node:fs'
+import path from 'node:path/posix'
 import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
 import { globSync } from 'glob'
+import { readConfigFile } from 'typescript'
+import type { LibraryOptions, Plugin } from 'vite'
 import { defineConfig } from 'vite'
-import type { LibraryOptions } from 'vite'
 import dts from 'vite-plugin-dts'
+
+/**
+ * Like the "vite-tsconfig-paths" package, but it cannot handle CSS @import alias.
+ * This plugin, instead, just convert tsconfig.json's compilerOptions.paths to
+ * vite's resolve.alias options rather than the resolveId hook
+ */
+function tsconfigAliasPlugin(): Plugin {
+    const { config } = readConfigFile('tsconfig.json', (path) =>
+        fs.readFileSync(path, 'utf-8'),
+    )
+    const { baseUrl, paths } = config.compilerOptions as {
+        baseUrl: string
+        paths: Record<string, string[]>
+    }
+    /** convert "src/*" to "src" */
+    const convert = (s: string) => s.replace(/\/\*$/, '')
+
+    const alias: Array<{ find: string; replacement: string }> = []
+    for (const [from, mappings] of Object.entries(paths)) {
+        for (const mapping of mappings) {
+            alias.push({
+                find: convert(from),
+                replacement: fileURLToPath(
+                    new URL(
+                        path.resolve(baseUrl, convert(mapping)),
+                        import.meta.url,
+                    ),
+                ),
+            })
+        }
+    }
+    console.debug(alias)
+    return {
+        name: 'tsconfigAliasPlugin',
+        config() {
+            return { resolve: { alias } }
+        },
+    }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
     css: {
         preprocessorOptions: {
             scss: {
@@ -36,7 +72,7 @@ export default defineConfig({
             cssFileName: 'style',
         },
         rollupOptions: {
-            external: ['react', 'react-dom', 'react/jsx-runtime','scheduler'],
+            external: ['react', 'react-dom', 'react/jsx-runtime', 'scheduler'],
             output: {
                 banner: `"use client";`,
                 manualChunks(id, { getModuleInfo }) {
@@ -57,6 +93,7 @@ export default defineConfig({
         },
     },
     plugins: [
+        tsconfigAliasPlugin(),
         react({
             jsxImportSource: '@emotion/react',
             babel: {
